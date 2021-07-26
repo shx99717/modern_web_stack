c5857cda6b7f2855cf24f3ad37aac01cbd673787
Raymond Shen <raymond.shen@medavis.com>
2021-07-26 17:17:50 +0800

    new content added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import './styles.css'
+import IMAGE from './react_logo.png'
+import BANANA from './bananas.svg'
+
 export const App = () => {
-    return <h1>React TypeScript WebPack Starter Template</h1>    
+    return <>
+    <h1>React TypeScript WebPack Starter Template</h1>
+    <img src={IMAGE} alt="React Logo" width="300" />
+    <img src={BANANA} alt="BANANA" width="300" />
+    </>
 }
