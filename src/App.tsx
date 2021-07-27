3951ea2fd0f00dc6ccc9f7a296d66a6a852077f7
Raymond <raymond.shen@medavis.com>
2021-07-27 23:45:25 +0800

    new content added

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import './styles.css'
 import './styles.scss'
 import IMAGE from './react_logo.png'
 import BANANA from './bananas.svg'
+import { Counter } from './ClickCounter'
 
 export const App = () => {
     return <>
-    <h1>React TypeScript WebPack Starter Template --- {process.env.NODE_ENV} | {process.env.name}</h1>
+    <h1>(3)React TypeScript WebPack Starter Template --- {process.env.NODE_ENV} | {process.env.name}</h1>
     <img src={IMAGE} alt="React Logo" width="300" />
     <img src={BANANA} alt="BANANA" width="300" />
+    <Counter />
     </>
 }
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,9 +1,15 @@
 const webpack = require('webpack')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 module.exports = {
   mode: 'development',
   devtool: 'cheap-module-source-map',
+  devServer: {
+    hot: true,
+    open: true,
+  },
   plugins: [
+      new ReactRefreshWebpackPlugin(),
       new webpack.DefinePlugin({
           'process.env.name': JSON.stringify('DEV environment')
       })
