0770c8a6f365cd463b99da0cdecc93f6d9ac959f
Raymond Shen <raymond.shen@medavis.com>
2021-07-28 15:19:05 +0800

    new content added

--- react-template/webpack/webpack.common.js
+++ react-template/webpack/webpack.common.js
@@ -21,11 +21,11 @@ module.exports = {
         test: /\.(s[ac]ss|css)$/,
         use: [
           // Creates `style` nodes from JS strings
-          'style-loader', 
+          'style-loader',
           // Translates CSS into CommonJS
           'css-loader',
-          // Compiles Sass to CSS 
-          'sass-loader'
+          // Compiles Sass to CSS
+          'sass-loader',
         ],
       },
       {
@@ -46,5 +46,5 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '..', './src/index.html'),
     }),
-  ]
+  ],
 }
--- react-template/webpack/webpack.prod.js
+++ react-template/webpack/webpack.prod.js
@@ -1,11 +1,14 @@
 const webpack = require('webpack')
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
   mode: 'production',
   devtool: 'source-map',
   plugins: [
     new webpack.DefinePlugin({
-        'process.env.name': JSON.stringify('Prod environment')
-    })
-]
+      'process.env.name': JSON.stringify('Prod environment'),
+    }),
+    new BundleAnalyzerPlugin(),
+  ],
 }
